41b91d0161f31f20afef79bcc16a9656b5a50dd3
glinos123 <lino58849@gmail.com>
2025-01-28 08:37:51 +0200

    add picture of profile

--- src/compunent/Nav.js
+++ src/compunent/Nav.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from '@mui/material'
 import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon"
 import { Link } from 'react-router-dom'
+import papitoProfile from "../image/papito profilr.jpg";
 
 export default function Nav() {
   return (
     <AppBar>
         <Toolbar>
             <IconButton size='large' edge="start" color='inherit' aria-label='logo'>
-                <CatchingPokemonIcon/>
+                <img style={{width:"50px", height:"50px", borderRadius:"50%"}} src={papitoProfile} alt='image is not availeble'/>
             </IconButton>
             <Typography variant='h6' component="div" sx={{flexGrow:1}}>
                 
